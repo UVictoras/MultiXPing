63f1cccb390332a5aec23d99fad2b60bf015117e
UVictoras <vmartinan@gaming.tech>
2024-03-19 15:57:51 +0100

    InputManager

---
+++ MultiXPing/source/Utilitaries/InputManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiXPing
+{
+    internal class InputManager
+    {
+        /* ----------------------------------------------------- *\
+        |                                                         |
+        |                          Field                          |
+        |                                                         |
+        \* ----------------------------------------------------- */
+        #region Field
+
+        Dictionary<string, bool> _keyState = new Dictionary<string, bool>();
+        List<ConsoleKeyInfo> _keysInfo = new List<ConsoleKeyInfo>();
+
+        #endregion Field
+
+        /* ----------------------------------------------------- *\
+        |                                                         |
+        |                        Property                         |
+        |                                                         |
+        \* ----------------------------------------------------- */
+        #region Property
+
+        #endregion Property
+
+        /* ----------------------------------------------------- *\
+        |                                                         |
+        |                          Event                          |
+        |                                                         |
+        \* ----------------------------------------------------- */
+        #region Event
+
+        #endregion Event
+
+        /* ----------------------------------------------------- *\
+        |                                                         |
+        |                         Methods                         |
+        |                                                         |
+        \* ----------------------------------------------------- */
+        #region Methods
+
+        #endregion Methods
+    }
+}
